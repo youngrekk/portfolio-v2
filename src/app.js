3f41a28eacd6b4dcb60833089460d8a4598a7e33
rodri <rodriwachas@gmail.com>
2022-09-28 23:19:27 -0300

    reinitialized files

---
+++ src/app.js
@@ -0,0 +1,19 @@
+window.addEventListener("scroll", function() {
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("sticky", window.scrollY > 0)
+})
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
